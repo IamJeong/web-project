01ffe3712b909fb399c2d3bf9f0341c09f217c5a
IamJeong <wqeeewee@gmail.com>
2024-07-18 07:14:06 +0900

    React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import TextImageProblem from './Components/TextImageProblem';
+import MathProblem from './Components/MathProblem'
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      <Route path = "/" element = {<TextImageProblem/>} />
+      <Route path = "/page2"element = {<MathProblem/>} />
+    </Routes>
+  )
 }
 
 export default App;
